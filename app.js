73ef4a7d56bc28d204d28625e2438b5de5a2f4af
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-05 16:30:55 +0530

    taking input and updating the controller

--- app.js
+++ app.js
@@ -5,18 +5,48 @@ var budgetController = (function(){
 
 // UI CONTROLLER
 var UIController = (function(){
+    var DOMStrings = {
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    }
+    return {
+        getInput:function(){
+            return {
+            type : document.querySelector(DOMStrings.inputType).value, // Will be inc or exp
+            description : document.querySelector(DOMStrings.inputDescription).value,
+            value : document.querySelector(DOMStrings.inputValue).value
+            }
+        },
 
+        getDOMstrings:function(){
+            return DOMStrings;
+        }
+    }
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function (){
-        console.log('It Works.');
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to budget calculator
+
+        // 3. Add the item to the UI
+
+        // 4. Calcualte the budget
+
+        // 5. Display the budget on the UI
 
     }
     
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
     document.addEventListener('keypress',function (event) {
         if(event.keyCode === 13 || event.which === 13){
             ctrlAddItem();
