6b66592412b6ac4697a3a808ed6c959aa14c6754
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-05 14:57:00 +0530

    added event listeners

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
+//  BUDGET CONTROLLER
 var budgetController = (function(){
-    var x = 23;
-    var add = function (a) {
-        return x+a;
-    }
-    return {
-        publicTest:function (b) {
-            return (add(b));
-        }
-    }
+
 })();
 
-
+// UI CONTROLLER
 var UIController = (function(){
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(10);
-    return {
-        anotherPublic : function () {
-            console.log(z);
-        }
-    }
 
+    var ctrlAddItem = function (){
+        console.log('It Works.');
+
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress',function (event) {
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
-
-
- 
-
-
-
-
-
-
-
-
 
